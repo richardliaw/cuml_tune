a0da8ff34e82a248a85c900c3bef6f98a35cb793
Richard Liaw <rliaw@berkeley.edu>
2020-02-19 18:27:02 -0800

    modifications

--- cuml_airline.py
+++ cuml_airline.py
@@ -37,59 +37,14 @@ del X_cudf_train
 
 y_cudf_train = cudf.Series(y_train.values)
 
-cuml_model = GPURandomForestClassifier(
-    n_estimators=40,
-    max_depth=6,
-    max_features=1.0,
-    seed=10)
 
-cuml_model.fit(X_cudf_train, y_cudf_train)
+cuml_model = GPURandomForestClassifier(
+    n_estimators=467,
+    max_depth=19,
+    max_features=1.0
+)
+
+cuml_model.fit(train_mat, y_cudf_train)
 
 fil_preds_orig = cuml_model.predict(X_cudf_test)
 fil_acc_orig = accuracy_score(y_test, fil_preds_orig)
-
-# data = prepare_dataset(dataset_folder, dataset, args.nrows)
-
-
-# from ray import tune
-# from ray.tune.utils import pin_in_object_store, get_pinned_object
-
-# data_id = pin_in_object_store([X_train, X_test, y_train, y_test])
-
-# class CUMLTrainable(tune.Trainable):
-#     def _setup(self, config):
-#         [X_train, X_test, y_train, y_test] = get_pinned_object(data_id)
-
-#         self.cuml_model = curfc(
-#             n_estimators=config.get("estimators", 40),
-#             max_depth=config.get("depth", 16),
-#             max_features=1.0
-#         )
-#         self.X_cudf_train = cudf.DataFrame.from_pandas(X_train)
-#         self.X_cudf_test = cudf.DataFrame.from_pandas(X_test)
-#         self.y_cudf_train = cudf.Series(y_train.values)
-#         self.y_test = y_test
-
-#     def _train(self):
-#         self.cuml_model.fit(
-#             self.X_cudf_train,
-#             self.y_cudf_train
-#         )
-#         fil_preds_orig = self.cuml_model.predict(
-#             self.X_cudf_test)
-#         return {"mean_accuracy": accuracy_score(self.y_test, fil_preds_orig)}
-
-#     def _stop(self):
-#         del self.X_cudf_train
-#         del self.X_cudf_test
-#         del self.y_cudf_train
-#         del self.y_test
-#         del self.cuml_model
-
-
-# analysis = tune.run(
-#     CUMLTrainable,
-#     resources_per_trial={"gpu": 0.3},
-#     num_samples=20,
-#     config={"depth": tune.choice(list(range(8, 24)))},
-#     stop={"training_iteration": 1}, verbose=1)
--- cuml_airline_tune.py
+++ cuml_airline_tune.py
@@ -17,12 +17,13 @@ X_train, X_test, y_train, y_test = data.X_train, data.X_test, data.y_train, data
 y_train = y_train.astype(np.int32)
 y_test = y_test.astype(np.int32)
 
-QUARTER = len(X_train) // 2
+QUARTER = len(X_train) // 3
 X_train = X_train[QUARTER:]
 y_train = y_train[QUARTER:]
 
 ray.init()
 data_id = pin_in_object_store([X_train, X_test, y_train, y_test])
+
 
 class CUMLTrainable(tune.Trainable):
     def _setup(self, config):
@@ -34,7 +35,8 @@ class CUMLTrainable(tune.Trainable):
         self.X_cudf_test = cudf.DataFrame.from_pandas(X_test)
         self.y_cudf_train = cudf.Series(y_train.values)
         self.y_test = y_test
-        self.cuml_model = GPURandomForestClassifier(**self.config)
+        config = {k: int(v) for k, v in config.items()}
+        self.cuml_model = GPURandomForestClassifier(**config)
 
     def _train(self):
         self.cuml_model.fit(self.train_mat, self.y_cudf_train)
@@ -44,18 +46,48 @@ class CUMLTrainable(tune.Trainable):
 
     def reset_config(self, config):
         del self.cuml_model
-        self.cuml_model = GPURandomForestClassifier(config)
+        config = {k: int(v) for k, v in config.items()}
+        self.cuml_model = GPURandomForestClassifier(**config)
+        return True
 
+def tune_randomsearch():
 
-analysis = tune.run(
-    CUMLTrainable,
-    resources_per_trial={"gpu": 1},
-    num_samples=10,
-    reuse_actors=True,
-    config={
-        "n_estimators": tune.choice(list(range(80, 500))),
-        "max_depth": tune.choice(list(range(8, 24))),
-    },
-    stop={"training_iteration": 1},
-    verbose=1
-)
+    analysis = tune.run(
+        CUMLTrainable,
+        resources_per_trial={"gpu": 1},
+        num_samples=10,
+        reuse_actors=True,
+        config={
+            "n_estimators": tune.choice(list(range(80, 500))),
+            "max_depth": tune.choice(list(range(8, 24))),
+        },
+        stop={"training_iteration": 1},
+        verbose=1
+    )
+
+def tune_hyperopt():
+    from hyperopt import hp
+    from ray.tune.suggest.hyperopt import HyperOptSearch
+
+    search_alg = HyperOptSearch(
+        space={
+            "n_estimators": hp.loguniform("n_estimators", 2, 6),
+            "max_depth": hp.uniform("max_depth", 2, 20),
+            "min_impurity_decrease":  hp.loguniform("min_impurity_decrease", -5, -1)
+        },
+        metric="mean_accuracy",
+        mode="max",
+        n_initial_points=5,
+        points_to_evaluate=[{"n_estimators": 100, "max_depth": 16, "min_impurity_decrease": 0}]
+    )
+
+    analysis = tune.run(
+        CUMLTrainable,
+        resources_per_trial={"gpu": 1},
+        num_samples=100,
+    #     reuse_actors=True,
+        stop={"training_iteration": 1},
+        verbose=1,
+        max_failures=0,
+        search_alg=search_alg
+    )
