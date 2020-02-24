afdc78d1c2f45aa711ad30e82712c5096ff3e6fc
Richard Liaw <rliaw@berkeley.edu>
2020-02-24 14:40:49 -0800

    py

--- cuml_airline_tune.py
+++ cuml_airline_tune.py
@@ -21,22 +21,28 @@ QUARTER = len(X_train) // 3
 X_train = X_train[QUARTER:]
 y_train = y_train[QUARTER:]
 
-ray.init()
-data_id = pin_in_object_store([X_train, X_test, y_train, y_test])
+# ray.init()
+# data_id = pin_in_object_store([X_train, X_test, y_train, y_test])
 
+import os
+from filelock import FileLock
 
 class CUMLTrainable(tune.Trainable):
     def _setup(self, config):
-        [X_train, X_test, y_train, y_test] = get_pinned_object(data_id)
+        # [X_train, X_test, y_train, y_test] = get_pinned_object(data_id)
+        self._gpu_id = os.environ.get("CUDA_VISIBLE_DEVICES", 0) #ray.get_gpu_ids()[0]
+        print("Starting new trainable on {}.".format(self._gpu_id))
+        # self._wait_for_gpus()
 
-        X_cudf_train = cudf.DataFrame.from_pandas(X_train)
-        self.train_mat = X_cudf_train.as_gpu_matrix(order="F")
-        del X_cudf_train
-        self.X_cudf_test = cudf.DataFrame.from_pandas(X_test)
-        self.y_cudf_train = cudf.Series(y_train.values)
-        self.y_test = y_test
-        config = {k: int(v) for k, v in config.items()}
-        self.cuml_model = GPURandomForestClassifier(**config)
+        with FileLock(os.path.expanduser("~/.tune.gpulock")):
+            X_cudf_train = cudf.DataFrame.from_pandas(X_train)
+            self.train_mat = X_cudf_train.as_gpu_matrix(order="F")
+            del X_cudf_train
+            self.X_cudf_test = cudf.DataFrame.from_pandas(X_test)
+            self.y_cudf_train = cudf.Series(y_train.values)
+            self.y_test = y_test
+            config = {k: int(v) for k, v in config.items()}
+            self.cuml_model = GPURandomForestClassifier(**config)
 
     def _train(self):
         self.cuml_model.fit(self.train_mat, self.y_cudf_train)
@@ -44,11 +50,39 @@ class CUMLTrainable(tune.Trainable):
         accuracy = accuracy_score(self.y_test, fil_preds_orig)
         return {"mean_accuracy": accuracy}
 
+    def _stop(self):
+        import time
+        import GPUtil
+        gpu_object = GPUtil.getGPUs()[self._gpu_id]
+        print("Deleting the model. Mem: {:0.3f}".format(gpu_object.memoryUsed))
+        del self.cuml_model
+        print("Deleting the test set. Mem: {:0.3f}".format(gpu_object.memoryUsed))
+        del self.X_cudf_test
+        print("Deleting the test labels. Mem: {:0.3f}".format(gpu_object.memoryUsed))
+        del self.y_test
+        print("Deleting the training labels. Mem: {:0.3f}".format(gpu_object.memoryUsed))
+        del self.y_cudf_train
+        print("Deleting the training matrix. Mem: {:0.3f}".format(gpu_object.memoryUsed))
+        del self.train_mat
+#         self._wait_for_gpus(retry=1)
+
+
+    def _wait_for_gpus(self, retry=10):
+        import GPUtil
+        import time
+        gpu_object = GPUtil.getGPUs()[self._gpu_id]
+        for i in range(int(retry)):
+            if gpu_object.memoryUsed > 0.1:
+                print("Waiting for GPU memory to free. Mem: {:0.3f}".format(gpu_object.memoryUsed))
+                time.sleep(5)
+        time.sleep(5)
+
     def reset_config(self, config):
         del self.cuml_model
         config = {k: int(v) for k, v in config.items()}
         self.cuml_model = GPURandomForestClassifier(**config)
         return True
+
 
 def tune_randomsearch():
 
@@ -58,7 +92,7 @@ def tune_randomsearch():
         num_samples=10,
         reuse_actors=True,
         config={
-            "n_estimators": tune.choice(list(range(80, 500))),
+            "n_estimators": tune.choice(list(range(80, 300))),
             "max_depth": tune.choice(list(range(8, 24))),
         },
         stop={"training_iteration": 1},
@@ -91,3 +125,14 @@ def tune_hyperopt():
         max_failures=0,
         search_alg=search_alg
     )
+
+if __name__ == '__main__':
+    for i in range(10):
+        config = {
+            "n_estimators": random.choice(list(range(80, 300))),
+            "max_depth": random.choice(list(range(8, 24))),
+        }
+        trainable = CUMLTrainable(config=config)
+        result = trainable.train()
+        trainable.stop()
+        print("Accuracy is", result.get("mean_accuracy"))
